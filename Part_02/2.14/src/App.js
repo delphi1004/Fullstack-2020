8e21f6e12aa679f985e39a8e2cf3af274edd5ead
jlsy_helsinki <jlsy012@MacBook-Pro.local>
2020-10-01 11:40:23 +0300

    added 2.14

--- Part_02/2.14/src/App.js
+++ Part_02/2.14/src/App.js
@@ -2,7 +2,36 @@ import React , {useState , useEffect} from 'react';
 import axios from 'axios'
 import './App.css';
 
-let countries = []
+const ShowWeather = ({capitalName}) => {
+
+  const [weatherData, setWeatherData] = useState()
+
+  console.log('ShowWeather')
+  
+  useEffect(() => {
+      const api_key = process.env.REACT_APP_API_KEY
+      const weatherQuery = 'http://api.weatherstack.com/current?access_key='+api_key+'&query='+capitalName
+      axios
+      .get(weatherQuery)
+      .then(response => {
+        console.log(response.data)
+        setWeatherData(response.data)
+      })
+    },[capitalName])
+
+  return(
+    <div>
+      <h3>Weather in {capitalName}</h3>
+      {weatherData &&
+        <div>
+          <p><b>temperature :</b>{weatherData.current.temperature} celcius</p>
+          <img src = {weatherData.current.weather_icons[0]} width = {100} alt = 'weather icon'/>
+          <p><b>wind :</b>{weatherData.current.wind_speed} mph direction {weatherData.current.wind_dir}</p>
+        </div>
+      }
+    </div>
+  )
+}
 
 const ShowCountries = ({foundCountries}) =>{
 
@@ -21,8 +50,8 @@ const ShowCountries = ({foundCountries}) =>{
             {foundCountries[0].languages.map((data,index) => <li key = {index}>{data.name}</li>)}
           </ul>
           <img src = {foundCountries[0].flag} width = {100}alt = {foundCountries[0].name}/>
+          <ShowWeather capitalName = {foundCountries[0].capital} />
         </div>
-        
       }
     </div>
   )
