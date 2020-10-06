e2e765b9309d3f6203085c2facfb13b31a076121
jlsy_helsinki <jlsy012@MacBook-Pro.local>
2020-10-06 13:25:50 +0300

    added build

---
+++ Part_03/3.11/build/precache-manifest.6a6d64e52e711efb1b6b0764208c4007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46150dc02ceefa3b5a577c1a7a0375d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce6efaed1fb29b1cf167",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b0aad06fc187fe579b9e",
+    "url": "/static/js/2.27023259.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27023259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce6efaed1fb29b1cf167",
+    "url": "/static/js/main.04be6a0f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
