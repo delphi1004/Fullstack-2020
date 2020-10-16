4c690dfc881460222652f760c71f416c8bf56445
jlsy_helsinki <jlsy012@MacBook-Pro.local>
2020-10-16 12:33:50 +0300

    added 4.22

---
+++ Part_04/4.22/tests/test_helper.js
@@ -0,0 +1,48 @@
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const supertest = require('supertest')
+const bcrypt = require('bcrypt')
+const app = require('../app')
+const api = supertest(app)
+
+const initialBlogs = [
+    { title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7 },
+    { title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5 },
+    { title: "Canonical string reduction", author: "Edsger W. Dijkstra", url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", likes: 12 },
+    { title: "First class tests", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", likes: 10 },
+    { title: "TDD harms architecture", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html", likes: 0 },
+    { title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", likes: 2 }
+]
+
+const initialUsers = [
+    { username: 'root', name: 'Superuser', password: '123' },
+    { username: 'delphi1004', name: 'John Lee', password: '1234' },
+    { username: 'apple', name: 'David Young', password: '12345' },
+    { username: 'orange', name: 'Jane Fonda', password: '123456' },
+    { username: 'silver', name: 'R2D2', password: '1234567' }    
+]
+
+const GetPasswordHash = (password) => {
+    return bcrypt.hashSync(password, 10) 
+}
+
+const BlogsInDb = async () => {
+    const blogs = await Blog.find({})
+    return blogs.map(blog => blog.toJSON())
+}
+
+const usersInDb = async () => {
+    const users = await User.find({})
+    return users.map(u => u.toJSON())
+}
+
+const SaveBlog = (newBlog) => {
+    return api.post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+}
+
+module.exports = {
+    initialBlogs, initialUsers , GetPasswordHash, BlogsInDb, SaveBlog, usersInDb
+}
---
+++ Part_04/4.22/tests/user_api.test.js
@@ -0,0 +1,169 @@
+const mongoose = require('mongoose')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+const supertest = require('supertest')
+const app = require('../app')
+const logger = require('../utils/logger')
+const api = supertest(app)
+const Helper = require('./test_helper')
+
+beforeEach(async () => {
+    console.log('clear users and add several users')
+    await User.deleteMany({})
+
+    const userObject = Helper.initialUsers.map(user => {
+        return new User({ ...user, passwordHash: Helper.GetPasswordHash(user.password) })
+    })
+
+    const promiseArray = userObject.map(user => user.save())
+    await Promise.all(promiseArray)
+})
+
+describe('Testing for adding user(s)', () => {
+
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+        const usersAtStart = await Helper.usersInDb()
+
+        const newUser = {
+            username: 'root',
+            name: 'Superuser',
+            password: 'salainen',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('`username` to be unique')
+
+        const usersAtEnd = await Helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await Helper.usersInDb()
+
+        const newUser = {
+            username: 'fresh',
+            name: 'Mike Kim',
+            password: '1234'
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await Helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(user => user.username)
+        expect(usernames).toContain(newUser.username)
+    })
+
+    test('validation with insufficient length of username', async () => {
+        const usersAtStart = await Helper.usersInDb()
+
+        const newUser = {
+            username: 'a',
+            name: 'John Lee',
+            password: '1234'
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('username must be logner')
+        const usersAtEnd = await Helper.usersInDb()
+        expect(usersAtEnd.length).toBe(usersAtStart.length)
+    })
+
+    test('validation with insufficient length of password', async () => {
+        const usersAtStart = await Helper.usersInDb()
+
+        const newUser = {
+            username: 'insufficient',
+            name: 'Tim Bolton',
+            password: '12'
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('Password validation failed')
+
+        const usersAtEnd = await Helper.usersInDb()
+        expect(usersAtEnd.length).toBe(usersAtStart.length)
+    })
+})
+
+describe('Testing for user logging', () => {
+
+    test('login validation', async () => {
+        const userIndex = Math.floor(Math.random() * Helper.initialUsers.length)
+        const selectedUser = Helper.initialUsers[userIndex]
+
+        const user = {
+            username: selectedUser.username,
+            password: selectedUser.password
+        }
+
+        const result = await api
+            .post('/api/login')
+            .send(user)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    
+        expect(result.body.username).toBe(selectedUser.username)
+        expect(result.body.name).toBe(selectedUser.name)
+    })
+
+    test('invalid login with empty password', async () => {
+        const userIndex = Math.floor(Math.random() * Helper.initialUsers.length)
+        const selectedUser = Helper.initialUsers[userIndex]
+
+        const user = {
+            username: selectedUser.username,
+            password:''
+        }
+
+        const result = await api
+            .post('/api/login')
+            .send(user)
+            .expect(401)
+            .expect('Content-Type', /application\/json/)
+    
+            expect(result.body.error).toContain('invalid username or password')
+    })
+
+    test('invalid login with wrong user name', async () => {
+        const userIndex = Math.floor(Math.random() * Helper.initialUsers.length)
+        const selectedUser = Helper.initialUsers[userIndex]
+
+        const user = {
+            username: 'dummy',
+            password: selectedUser.password
+        }
+
+        const result = await api
+            .post('/api/login')
+            .send(user)
+            .expect(401)
+            .expect('Content-Type', /application\/json/)
+    
+            expect(result.body.error).toContain('invalid username or password')
+    })
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
